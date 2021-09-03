a302ec8104f836e239d886ff2cd3df1ca11a89d6
shanmugapriya2872 <79776417+shanmugapriya2872@users.noreply.github.com>
2021-09-03 21:32:42 +0530

    Update charoccurance.py

--- charoccurance.py
+++ charoccurance.py
@@ -1,6 +1,4 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def check(word):
+  def check(word):
     new=word
     for i in range(len(word)):
         count=word.count(word[i])
